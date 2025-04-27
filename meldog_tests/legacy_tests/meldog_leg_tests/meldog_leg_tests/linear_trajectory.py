b5b193e0a64d5baa68f3725c34132bbc293fb8c9
Bartek <bartlomiejk@vp.pl>
2025-04-27 21:06:00 +0200

    Added packages from main to fix it

---
+++ meldog_tests/legacy_tests/meldog_leg_tests/meldog_leg_tests/linear_trajectory.py
@@ -0,0 +1,67 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Vector3
+import math
+from rclpy.duration import Duration
+import rclpy.time
+
+class Linear_Trajectory(Node):
+    def __init__(self,name):
+        super().__init__(name)
+        self.declare_parameter("start_position", [0.0, -0.30])
+        self.declare_parameter("radius",0.1)
+        self.declare_parameter("angular_velocity", 6 * 3.14)
+        self.x = self.get_parameter("start_position").value[0]
+        self.y = self.get_parameter("start_position").value[1]
+        self.radius = self.get_parameter("radius").value
+        self.angular_velocity = self.get_parameter("angular_velocity").value
+
+        self.logger = self.get_logger()
+
+        self.clock = self.get_clock()
+        self.time_prev = self.clock.now()
+
+
+        period_seconds = math.floor(2*math.pi/self.angular_velocity)
+        period_nanoseconds = int((2*math.pi/self.angular_velocity - period_seconds)*10**9)
+        self.cycle_period = Duration(seconds = period_seconds,
+                                            nanoseconds = period_nanoseconds)
+
+        self.end_effector_position = Vector3()
+
+
+        self.wait_for_solver()
+        self.publisher_ = self.create_publisher(Vector3, 'end_effector_desired_trajectory', 10)
+        self.timer_period = 0.01  # seconds
+        self.timer = self.create_timer(self.timer_period, self.trajectory_callback)
+
+    def trajectory_callback(self):
+        self.time_now = self.clock.now()
+        cycle_duration = (self.time_now - self.time_prev)
+        cycle_duration_seconds= cycle_duration.nanoseconds/10**9 
+        self.end_effector_position.x = 0.0
+        self.end_effector_position.y = self.y + self.radius*math.sin(self.angular_velocity*cycle_duration_seconds)
+        self.end_effector_position.z = 0.0
+        if(cycle_duration > self.cycle_period):
+            self.time_prev = self.time_now
+            self.logger.info("Cycle ended!")
+        self.publisher_.publish(self.end_effector_position)
+
+    def wait_for_solver(self): # Czeka chwilę, aż noga ustawi się w pozycji startowej
+        wait_time = Duration(seconds = 2)
+        while(wait_time > (self.clock.now() - self.time_prev)):
+            self.clock.sleep_for(Duration(nanoseconds = 10**8))
+        self.logger.info("Linear trajectory generation has started!")
+        self.time_prev = self.clock.now()
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Linear_Trajectory("linear_trajectory")
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ meldog_tests/legacy_tests/meldog_leg_tests/meldog_leg_tests/moteus_ploter.py
@@ -0,0 +1,100 @@
+import rclpy
+from rclpy.node import Node
+from meldog_interfaces_tests.msg import MultiMoteusControl, MultiMoteusState
+from meldog_interfaces_tests.msg import MoteusControl, MoteusState
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import threading
+class Ploter(Node):
+
+    def __init__(self, name, plt: plt):
+        super().__init__(name)
+        self.logger = self.get_logger()
+        self.declare_parameter("variable", "position")
+        self.declare_parameter("which_motor", 1)
+        self.variable = self.get_parameter("variable").value
+        self.which_motor = self.get_parameter("which_motor").value
+        self.y_label = ""
+        if(self.variable == "position"):
+             self.y_label = "Pozycja złącza [rad]"
+        elif(self.variable == "velocity"):
+             self.y_label = "Prędkość kątowa złącza [rad/s]"
+        elif(self.variable == "torque"):
+             self.y_label = "Moment złącza [Nm]"
+        self.control_subscriber = self.create_subscription(MultiMoteusControl,'multi_moteus_control',self.plot_control,10)
+        self.state_subscriber = self.create_subscription(MultiMoteusState,'multi_moteus_state',self.plot_state,10)
+        self.timer = self.create_timer(0.01, self.update_plot)
+        self.time_list = []
+        self.control_array = []
+        self.state_array = []
+        self.control_value = 0
+        self.state_value = 0
+        self.relative_error = 0
+        self.relative_error_array = []
+        self.figure = plt.figure()
+        self.ax = self.figure.add_subplot(111)
+        
+        self.logger.info("Ploter initialized!")
+    def plot_control(self, msg):
+            self.control_value = msg.control_array[self.which_motor - 1].desired_position
+        
+    def plot_state(self, msg):       
+            self.state_value = msg.state_array[self.which_motor - 1].position
+    def update_plot(self):
+         self.state_array.append(self.state_value)
+         self.control_array.append(self.control_value)
+
+         self.relative_error = abs((self.control_value-self.state_value)/(self.control_value+0.001))
+         self.relative_error_array.append(self.relative_error)
+
+    def animate_2(self, i, data_list_1, data_list_2):
+        data_list_1 = data_list_1[-100:]
+        data_list_2 = data_list_2[-100:]
+        if(data_list_1 is None and data_list_2 is None):
+             max_value = 1
+             min_value = -1
+        else:
+            max_value = max([max(data_list_1),max(data_list_2)])
+            min_value = min([min(data_list_1),min(data_list_2)])
+            if(max_value >= 0):
+                 max_value *= 1.2
+            else:
+                 max_value *= 0.8
+            if(min_value <= 0):
+                 min_value *= 1.2
+            else:
+                 min_value *= 0.8
+        self.ax.clear()
+        self.ax.set_ylim([min_value, max_value])
+        self.ax.set_ylabel(self.y_label )
+        self.ax.plot(data_list_1, label = "ROS")
+        self.ax.plot(data_list_2, label = "Silnik")
+        self.ax.legend()
+    def animate_1(self, i, data_list):
+        data_list = data_list[-100:]
+        self.ax.clear()
+        self.ax.set_ylim([-2, 2])
+        self.ax.set_ylabel("Blad wzgledny")
+        self.ax.plot(data_list)
+    
+def main(args=None):
+    rclpy.init(args=args)
+    node = Ploter("Ploter_node", plt)
+    def start_ros():
+        rclpy.spin(node)
+        node.destroy_node()
+        rclpy.shutdown()
+    
+    ros_thread = threading.Thread(target=start_ros)
+    ros_thread.start()
+    #ani_1 = animation.FuncAnimation(node.figure, node.animate_1, fargs=(node.relative_error_array), interval = 50)
+    ani_2 = animation.FuncAnimation(node.figure, node.animate_2, fargs=(node.control_array, node.state_array), interval = 50)
+    plt.show()
+
+    
+
+
+if __name__ == '__main__':
+    main()
+
+
